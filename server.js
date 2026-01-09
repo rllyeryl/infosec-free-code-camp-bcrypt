da9675cbbce2aee73baeb6eb726a75c5843aa877
rllyeryl <sendtoejana@gmail.com>
2026-01-09 12:49:54 +0800

    added and compared hash

--- server.js
+++ server.js
@@ -2,14 +2,35 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-// NEW 1. required bcrypt
+// 1. required bcrypt
 const bcrypt = require('bcrypt');
 
 const app         = express();
 fccTesting(app);
+
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+
+// NEW 2. generate the hash
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Hashed Password: ' + hash);
+
+    // NEW 3. hash compare
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Result of comparison: ' + res); // This should log 'true'
+      }
+    });
+  }
+});
+
+
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
